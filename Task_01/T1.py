49f8c38136fb5a2d15003b048ebc56261f647e02
Evgeniy Bratenkov <giertrut@yandex.ru>
2023-03-11 16:32:07 +0300

    1

--- Task_01/T1.py
+++ Task_01/T1.py
@@ -3,10 +3,15 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) 
 
-num = int(input("Введите трехзначное число: "))
-total = 0
-for i in range(3):
-    last_num = num % 10
-    total += last_num
-    num //= 10
-print(total) 
+# num = int(input("Введите трехзначное число: "))
+# total = 0
+# for i in range(3):
+#     last_num = num % 10
+#     total += last_num
+#     num //= 10
+# print(total) 
+
+
+a = int(input("Введите трехзначное число: "))
+b = str(a)
+print(int(b[0]) + int(b[1]) + int(b[2]))
